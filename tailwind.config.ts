db7ebbc6dbfbe971fc5fcd9b8cc0ae6dcfb2689f
Steven Yu <stevenyuser@gmail.com>
2024-01-26 03:34:40 -0500

    finished everything

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        slideDown: {
+          "0%": { transform: "translateY(1.75rem)" },
+          "100%": { transform: "translateY(0px)" },
+        },
+    },
+    animation: {
+      slideDown: "slideDown 0.25s ease-in-out",
+    },
     },
   },
   plugins: [],
